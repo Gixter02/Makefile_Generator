d547afafc78400e9b8aa2c10feb57789fe2c3b2b
Gixter02 <mattia.montanari2002@gmail.com>
2023-05-18 18:17:38 +0200

    Wrote the README Added more controls in the C file

--- Makefile-Generator.c
+++ Makefile-Generator.c
@@ -1,30 +1,35 @@
-#include <stdio.h> /* Standard Input-Output */
-/* include necessario per usare printf e altre; inoltre
-definisce la costante BUFSIZ */
-#include <fcntl.h> /* File CoNTroL */
-/* include necessario per usare la primitiva open e le
-relative costanti come O_RDONLY */
-#include <stdlib.h> /* Standard Library */
-/* include necessario per usare la primitiva exit */
-#include <unistd.h> /* UNIX Standard */
-/* include necessario per usare la primitiva read, write
-e close */
-#include <sys/wait.h>
-/* include necessario per la primitiva wait */
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+
 
 int main(int argc, char ** argv){
     if(argc != 3){
         printf("ERRORE:Numero di parametri diverso da 3\n");
         exit(1);
     }
-    printf("Il file .c si chiama:\t%s\n", argv[1]);
-    printf("Il file eseguibile si chiamera':\t%s\n", argv[2]);
+    int clen = strlen(argv[1]);
+    if(clen <= 2){
+        printf("ERRORE: nome primo parametro non corretto\n");
+        exit(2);
+    }
+    if(argv[1][clen-1] != 'c' || argv[1][clen-2] != '.'){
+        printf("ERRORE: la stringa %s non termina con .c\n", argv[1]);
+        exit(3);
+    }
+    printf("Il file .c si chiama: \"%s\"\n", argv[1]);
+    printf("Il file eseguibile si chiamera': \"%s\"\n", argv[2]);
     FILE* f= fopen("makefile", "w");
     if(f==NULL){
         printf("ERRORE nell'apertura del file\n");
-        exit(2);
+        exit(4);
     }
     fprintf(f,"%s: %s\n\tgcc -Wall -std=c11 -o %s %s", argv[2], argv[1], argv[2], argv[1]);
-    fclose(f);
+    if(fclose(f) != 0){
+        printf("ERRORE: file chiuso NON correttamente\n");
+
+        exit(5);
+    }
+    printf("makefile creato correttamente!\n");
     exit(0);
 }
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Makefile_Generator
-This script will create a makefile to compile C files on UNIX
+# Makefile_Generator_for_UNIX
+Questo programma creerà un file chiamato *makefile* nella directory corrente, con al suo interno queste parole
+
+```
+file_eseguibile: file.c
+	gcc -Wall -std=c11 -o file_eseguibile file.c
+```
+
+Il programma funziona solo se il file .c è nella directory in cui invochiamo il programma Makefile-Generator.c
+
+**N.B.** In caso ce ne sia già uno presente nella stessa directory con lo stesso nome il file verrà sovrascritto
+
+# Istruzioni
+
+1. **Solo la prima volta** creare il file eseguibile Makefile-Generator compilando il file Makefile-Generator.c
+2. Invocare il programma Makefile-Generator con il comando `./Makefile-Generator file.c file_eseguiblie`, ogni qual volta su vuole creare un *makefile*
